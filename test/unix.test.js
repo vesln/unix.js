86092016b85b3d4e7ec7b3a1d448da6d1198050a
Veselin Todorov <veselin@veselin.bg>
2012-10-12 22:01:43 +0300

    Assert on exit code

--- test/unix.test.js
+++ test/unix.test.js
@@ -6,31 +6,53 @@
 var unix = require('..');
 
 describe('unix', function() {
-  it('can assert output', function(done) {
-    unix()
-      .run('ls Readme.md')
-      .prints('Readme.md\n')
+  describe('when asserting on stdout', function() {
+    it('does not return an error when the output matches the expected text', function(done) {
+      unix()
+      .run('ls ' + __filename)
+      .stdout(__filename + '\n')
       .end(function(err) {
         (err === null).should.be.true;
         done();
       });
-  });
+    });
 
-  it('returns an error if the output is not correct', function(done) {
-    unix()
-      .run('ls Readme.md')
-      .prints('lols')
+    it('returns an error if the output does not match the expected text', function(done) {
+      unix()
+      .run('ls ' + __filename)
+      .stdout('Invalid')
       .end(function(err) {
         err.message.should.eq('Expected "ls Readme.md" to print "lols" but it printed "lols"');
         done();
       });
+    });
+  });
+
+  describe('when asserting on exit code', function() {
+    it('does not return an error if the status code matches', function(done) {
+      unix()
+      .run('ls ' + __filename)
+      .code(0)
+      .end(function(err) {
+        (err === null).should.be.true;
+        done();
+      });
+    });
+
+    it('returns an err if the status code does not match', function(done) {
+      unix()
+      .run('ls /this-is-invalid')
+      .code(0)
+      .end(function(err) {
+        err.message.should.eq('Expected "ls Readme.md" to exit with "0" but it was "1"');
+        done();
+      });
+    });
   });
 
   xit('can ignore colors');
   xit('can ignore new lines');
-  xit('can assert on status code');
   xit('can assert on stderr');
-  xit('can assert on execution time');
   xit('supports timeouts');
   xit('supports env vars for a command');
   xit('throws an error if no command is supplied');
